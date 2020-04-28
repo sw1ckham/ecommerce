f4a1f1393aa19a066c4577eb1d52208a32ef0f1e
sw1ckham <sphwick@gmail.com>
2020-04-28 10:48:44 +0000

    Added heroku allowed database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), 'localhost']
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), 'localhost', 'mini-ecommerce.herokuapp.com']
 
 
 # Application definition
